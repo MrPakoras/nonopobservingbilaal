3bd9cee42ee62e4371883655bc4ee6b14791bd3b
MrPakoras <hartanas8g@gmail.com>
2020-11-30 09:02:16 +0000

    Updated Progress Bar

--- BN Bar.py
+++ BN Bar.py
@@ -33,15 +33,12 @@ COMMIT_MESSAGE = 'Updated Progress Bar'
 
 print('Pushing to Github...')
 
-def git_push():
-    try:
-        repo = Repo(PATH_OF_GIT_REPO)
-        repo.git.add(update=True)
-        repo.index.commit(COMMIT_MESSAGE)
-        origin = repo.remote(name='origin')
-        origin.push()
-        print('Done.')
-    except:
-        print('Some error occured while pushing the code')    
-
-git_push()
+try:
+	repo = Repo(PATH_OF_GIT_REPO)
+	repo.git.add(update=True)
+	repo.index.commit(COMMIT_MESSAGE)
+	origin = repo.remote(name='origin')
+	origin.push()
+	print('Done.')
+except:
+	print('Some error occured while pushing the code')
